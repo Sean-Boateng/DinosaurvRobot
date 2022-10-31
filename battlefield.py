1b6b83c153b12adbce95fdab7fa9df9468ed059c
Sean <seanboateng2@gmail.com>
2022-10-31 12:26:25 -0400

    files have been updated

---
+++ battlefield.py
@@ -0,0 +1,23 @@
+
+
+from DinosaurvRobot.dino import Dinosaur
+from DinosaurvRobot.robot import Robot
+
+
+class Battlefield() :
+    def __init__(self):
+        self.robot = Robot
+        self.dinosaur = Dinosaur
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
+    
